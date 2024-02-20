312b3b390aa3e019f3075ebd066cad292d3799af
CSIRamone <cidinei_inacio@hotmail.com>
2024-02-20 10:34:09 -0300

    Componente Message Bubble#2

--- chat/lib/components/message_bubble.dart
+++ chat/lib/components/message_bubble.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:chat/core/models/chat_messages.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
+  static const _defaultImage = 'assets/image/user.png';
   final ChatMessage message;
   final bool belongToCurrentUser;
   const MessageBubble({
@@ -10,49 +13,83 @@ class MessageBubble extends StatelessWidget {
     required this.belongToCurrentUser,
   });
 
+  Widget _showUserImage(String imageURL) {
+    ImageProvider? provider;
+
+    final uri = Uri.parse(imageURL);
+    if (uri.path.contains(_defaultImage)) {
+      provider = const AssetImage(_defaultImage);
+    } else if (uri.scheme.contains('http')) {
+      provider = NetworkImage(uri.toString());
+    } else {
+      provider = FileImage(File(uri.toString()));
+    }
+
+    return CircleAvatar(
+      backgroundImage: provider,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment:
-          belongToCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
       children: [
-        SizedBox(
-          width: 10,
-          height: 10,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color:
-                belongToCurrentUser ? Colors.cyan : Theme.of(context).hintColor,
-            borderRadius: const BorderRadius.all(
-              Radius.circular(12),
-            ),
-          ),
-          width: 180,
-          padding: const EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 16,
-          ),
-          child: Column(children: [
-            Text(
-              message.userName,
-              style: TextStyle(
-                color:
-                    belongToCurrentUser ? Colors.red.shade900 : Colors.white70,
-                fontWeight: FontWeight.bold,
+        Row(
+          mainAxisAlignment: belongToCurrentUser
+              ? MainAxisAlignment.end
+              : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: belongToCurrentUser
+                      ? Colors.cyan
+                      : Theme.of(context).hintColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: const Radius.circular(12),
+                    topRight: const Radius.circular(12),
+                    bottomLeft: belongToCurrentUser
+                        ? const Radius.circular(12)
+                        : const Radius.circular(0),
+                    bottomRight: belongToCurrentUser
+                        ? const Radius.circular(0)
+                        : const Radius.circular(12),
+                  )),
+              width: 180,
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
               ),
-            ),
-            Text(
-              message.text,
-              style: TextStyle(
-                color:
-                    belongToCurrentUser ? Colors.red.shade900 : Colors.white70,
+              margin: const EdgeInsets.symmetric(
+                vertical: 15,
+                horizontal: 8,
               ),
+              child: Column(children: [
+                Text(
+                  message.userName,
+                  style: TextStyle(
+                    color: belongToCurrentUser
+                        ? Colors.red.shade900
+                        : Colors.white70,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  message.text,
+                  style: TextStyle(
+                    color: belongToCurrentUser
+                        ? Colors.red.shade900
+                        : Colors.white70,
+                  ),
+                ),
+              ]),
             ),
-          ]),
+          ],
         ),
-        SizedBox(
-          width: 10,
+        Positioned(
+          top: 0,
+          left: belongToCurrentUser ? null : 165,
+          right: belongToCurrentUser ? 165 : null,
+          child: _showUserImage(message.userImageURL),
         )
       ],
     );
